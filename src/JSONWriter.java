cb1b7e34c47ba473e52c3926b007bcb0e7010623
Jeremy <jjkerby@dons.usfca.edu>
2017-02-13 04:16:31 -0800

    Writing works as expected. Need to handle nested files or more than one.

---
+++ src/JSONWriter.java
@@ -0,0 +1,221 @@
+
+/*
+ * Jeremy Kerby
+ * Imported from Lab: JSONWriter
+ */
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class JSONWriter {
+
+	/**
+	 * Returns a String with the specified number of tab characters.
+	 *
+	 * @param times
+	 *            number of tab characters to include
+	 * @return tab characters repeated the specified number of times
+	 */
+
+	public static String indent(int times) {
+		char[] tabs = new char[times];
+		Arrays.fill(tabs, '\t');
+		return String.valueOf(tabs);
+	}
+
+	/**
+	 * Returns a quoted version of the provided text.
+	 *
+	 * @param text
+	 *            text to surround in quotes
+	 * @return text surrounded by quotes
+	 */
+	public static String quote(String text) {
+		return String.format("\"%s\"", text);
+	}
+
+	/**
+	 * Writes the set of elements as a JSON array at the specified indent level.
+	 *
+	 * @param writer
+	 *            writer to use for output
+	 * @param elements
+	 *            elements to write as JSON array
+	 * @param level
+	 *            number of times to indent the array itself
+	 * @throws IOException
+	 */
+	private static void asArray(Writer writer, TreeSet<Integer> elements, int level) throws IOException {
+		/*
+		 * TODO This is optional, but if you implement this method you can reuse
+		 * it in several places in this class.
+		 */
+		writer.append("[\n");
+		System.out.print("[\n");
+
+		Integer data[] = elements.toArray(new Integer[elements.size()]);
+
+		for (int i = 0; i < data.length; i++) {
+			if (i != data.length - 1) {
+				writer.append(indent(level + 1) + data[i] + ",\n");
+				System.out.print(indent(level + 1) + data[i] + ",\n");
+			} else {
+				writer.append(indent(level + 1) + data[i] + "\n");
+				System.out.print(indent(level + 1) + data[i] + "\n");
+			}
+		}
+
+		writer.append(indent(level) + "]\n");
+		System.out.print(indent(level) + "]\n");
+
+	}
+
+	/**
+	 * Writes the set of elements as a JSON array to the path using UTF8.
+	 *
+	 * @param elements
+	 *            elements to write as a JSON array
+	 * @param path
+	 *            path to write file
+	 * @throws IOException
+	 */
+	public static void asArray(TreeSet<Integer> elements, Path path) throws IOException {
+		// TODO
+		try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+
+			int level = 1;
+
+			asArray(writer, elements, level);
+
+		}
+	}
+
+	/**
+	 * Writes the map of elements as a JSON object to the path using UTF8.
+	 *
+	 * @param elements
+	 *            elements to write as a JSON object
+	 * @param path
+	 *            path to write file
+	 * @throws IOException
+	 */
+	public static void asObject(TreeMap<String, Integer> elements, Path path) throws IOException {
+		// TODO Use try-with-resources (no catch block needed)
+		try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+
+			int level = 1;
+
+			// System.out.print("{\n");
+			writer.write("{\n");
+
+			int size = elements.size();
+			int n = 1;
+
+			Set<String> keys = elements.keySet();
+			for (String key : keys) {
+				if (n == size) {
+					// System.out.print(indent(level) + quote(key) + ": " +
+					// elements.get(key) + "\n");
+					writer.write(indent(level) + quote(key) + ": " + elements.get(key) + "\n");
+				} else {
+					// System.out.print(indent(level) + quote(key) + ": " +
+					// elements.get(key) + ",\n");
+					writer.write(indent(level) + quote(key) + ": " + elements.get(key) + ",\n");
+					n++;
+				}
+			}
+
+			// System.out.print("}\n");
+			writer.write("}");
+
+		}
+
+	}
+
+	/**
+	 * Writes the set of elements as a JSON object with a nested array to the
+	 * path using UTF8.
+	 *
+	 * @param elements
+	 *            elements to write as a JSON object with a nested array
+	 * @param path
+	 *            path to write file
+	 * @throws IOException
+	 */
+	public static void asNestedObject(TreeMap<String, TreeMap<String, TreeSet<Integer>>> elements, Path path)
+			throws IOException {
+		// TODO Use try-with-resources (no catch block needed)
+		try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+
+			Set<String> keys = elements.keySet();
+
+			int size = keys.size();
+			int k = 1;
+			System.out.print("{\n");
+			writer.append("{\n");
+			for (String key : keys) {
+				System.out.print(indent(1) + quote(key) + ": {");
+				writer.append(indent(1) + quote(key) + ": {");
+				TreeMap<String, TreeSet<Integer>> innerData = elements.get(key);
+				Set<String> innerKeys = innerData.keySet();
+				for (String ik : innerKeys) {
+					System.out.print("\n" + indent(2) + quote(ik) + ": ");
+					writer.append("\n" + indent(2) + quote(ik) + ": ");
+					asArray(writer, innerData.get(ik), 2);
+					if (k == size-1) {
+						System.out.print(indent(1) + "},\n");
+						writer.append(indent(1) + "},\n");
+						k++;
+					} else {
+						System.out.print(indent(1) + "}\n");
+						writer.append(indent(1) + "}\n");
+					}
+				}
+			}
+			System.out.print("}\n");
+			writer.append("}\n");
+
+			/*
+			 * int level = 2;
+			 * 
+			 * System.out.print(indent(level-2) + "{\n");
+			 * writer.append(indent(level-2) + "{\n");
+			 * 
+			 * int size = elements.size(); int n = 1;
+			 * 
+			 * 
+			 * 
+			 * Set<String> keys = elements.keySet(); for (String key : keys) {
+			 * TreeSet<Integer> myset = elements.get(key); if (n == size) {
+			 * //System.out.print(">>myset : " + myset);
+			 * System.out.print(indent(level) + quote(key) + ": ");
+			 * writer.append(indent(level) + quote(key) + ": ");
+			 * 
+			 * asArray(writer, myset, 2);
+			 * 
+			 * System.out.print("\n"); writer.append("\n");
+			 * 
+			 * 
+			 * } else { //System.out.print(">>myset : " + myset);
+			 * System.out.print(indent(level) + quote(key) + ": ");
+			 * writer.append(indent(level) + quote(key) + ": ");
+			 * 
+			 * asArray(writer, myset, 2);
+			 * 
+			 * System.out.print(",\n"); writer.append(",\n"); n++;
+			 * 
+			 * }
+			 * 
+			 * }
+			 */
+		}
+	}
+}
--- src/WordIndexWriter.java
+++ src/WordIndexWriter.java
@@ -1,13 +1,5 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 
 /**
  * Convenience class to write a {@link WordIndex} to a text file.
@@ -16,13 +8,12 @@ public class WordIndexWriter {
 
 	public static void writeIndex(WordIndex index, Path path) throws IOException {
 
-		TreeMap<String, TreeMap<String, TreeSet<Integer>>> data;
-		data = index.get(); // now we has the word index as object not as string
-
 		System.out.println(">> writeIndex() >> start");
 
 		System.out.println(">> writeIndex() >> My data is still " + index.toString());
-		
+
+		JSONWriter.asNestedObject(index.get(), path);
+
 		System.out.println(">> writeIndex() >> end");
 
 	}
